ccde57f7af380ec1b6cf91eeca60da48d9984cb4
Mateusz Zawistowski <maturasisko@gmail.com>
2019-11-19 15:03:48 +0100

    Initial commit

---
+++ CodecoolQuest/Models/Actors/Actor.cs
@@ -0,0 +1,28 @@
+﻿namespace Codecool.Quest.Models.Actors
+{
+    public abstract class Actor : IDrawable
+    {
+        public Cell Cell { get; private set; }
+        public int Health { get; set; } = 10;
+
+        public int X => Cell.X;
+
+        public int Y => Cell.Y;
+
+        public abstract string TileName { get; }
+
+        protected Actor(Cell cell)
+        {
+            Cell = cell;
+            Cell.Actor = this;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            var nextCell = Cell.GetNeighbor(dx, dy);
+            Cell.Actor = null;
+            nextCell.Actor = this;
+            Cell = nextCell;
+        }
+    }
+}
